f9221c8435e5886114430b50f1ec6df1c4b449a6
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-11-14 10:31:07 +0100

    comments

--- lektion1/create_table.sql
+++ lektion1/create_table.sql
@@ -1,16 +1,25 @@
+-- Skapar tabellen users med kolumnerna username och password.
+-- users och password har datatypen TEXT.
 CREATE TABLE users(username TEXT, password TEXT);
+
+-- Sätter in värdena Kristian och 123 i users, 
+-- värdena förekommer i samma ordning som kolumner
 INSERT INTO users
 VALUES ('Kristian', '123');
+
+-- Vi tar bort tabellen users. Finns ingen undo.
 DROP TABLE users;
+
 CREATE TABLE users(
     username TEXT,
-    password TEXT NOT NULL,
-    PRIMARY KEY (username)
+    password TEXT NOT NULL, -- password kan nu inte vara null
+    PRIMARY KEY (username) --username blir nu primary key
 );
 
 INSERT INTO users VALUES ('thomas', '123');
 
-SELECT * FROM employees;
+SELECT * FROM employees; -- Väljer alla rader från employees
+-- stjärnan innebär att det är alla kolumner som väljs.
 /*
  students(_email_, name)
  courses(_code_, name, yh_points)
