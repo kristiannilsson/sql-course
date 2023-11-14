d4f4951831855ac5d9482d697a2cacc523fa1145
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-11-14 10:21:57 +0100

    demo

--- lektion1/create_table.sql
+++ lektion1/create_table.sql
@@ -1,19 +1,32 @@
+CREATE TABLE users(username TEXT, password TEXT);
+INSERT INTO users
+VALUES ('Kristian', '123');
+DROP TABLE users;
+CREATE TABLE users(
+    username TEXT,
+    password TEXT NOT NULL,
+    PRIMARY KEY (username)
+);
 
+INSERT INTO users VALUES ('thomas', '123');
 
-    /*
-     students(_email_, name)
-     courses(_code_, name, yh_points)
-     grades(_student_, _course_, grade)
-     student --> students.email
-     course --> courses.code
-     
-     1. skapa en table users (prata om casing)
-     2. sätt in värden med INSERT INTO
-     3. DROP TABLE users
-     4. PRIMARY KEY
-     5. NULLS och NOT NULL
-     6. SELECT *
-     7. FOREIGN KEY
-     
-     
-     */
+SELECT * FROM employees;
+/*
+ students(_email_, name)
+ courses(_code_, name, yh_points)
+ grades(_student_, _course_, grade)
+    student --> students.email
+    course --> courses.code
+ I grades finns det två primary keys då en student kan ha flera betyg.
+ Men bara ett betyg i en kurs.
+ 
+ 1. skapa en table users (prata om casing)
+ 2. sätt in värden med INSERT INTO
+ 3. DROP TABLE users
+ 4. PRIMARY KEY
+ 5. NULLS och NOT NULL
+ 6. SELECT *
+ 7. FOREIGN KEY
+ 
+ 
+ */
